04374f803ccb979827065c855ffb40a97de2690d
Pavan Sohith <pavansohith@gmail.com>
2024-08-06 00:17:55 +0530

    Added pulltorefresh, appbar before loading

--- src/components/Blogs.tsx
+++ src/components/Blogs.tsx
@@ -9,15 +9,22 @@ export function Blogs() {
 
   if (loading) {
     return (
+      <div>
+              <AppBar />
+
       <div className="w-full h-screen items-center flex justify-center">
       
         <CubeLoader />
+        </div>
         </div>
     );
   }
   return (
     <div>
-      <AppBar />
+            <AppBar />
+
+            <div className="text-xl text-center h-4 w-full flex justify-center mb-4 items-center">Pull to refresh <DownArrow /> </div>
+
       <div className="flex flex-col justify-center items-center l:mt-8 z-20 mr-3">
         {blogs.map((blog) => (
           <BlogCard
@@ -35,4 +42,12 @@ export function Blogs() {
       </div>
     </div>
   );
+}
+
+
+function DownArrow() {
+  return <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 17.25 12 21m0 0-3.75-3.75M12 21V3" />
+</svg>
+
 }
